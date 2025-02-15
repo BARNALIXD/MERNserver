10a454ca8fcfedda69aadf0122ea74faefaa46d4
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-15 16:34:23 +0530

    3:19:17 cloudinary fixations

---
+++ helpers/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+
+//configure the cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadMediaToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
